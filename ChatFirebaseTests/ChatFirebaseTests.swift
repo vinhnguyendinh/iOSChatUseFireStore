f3f22b5555cebaa1e5973c18b69b33ea1e86cb81
Nguyen Vinh <vinhnd@ominext.com>
2019-01-18 23:53:58 +0700

    Initial Commit

---
+++ ChatFirebaseTests/ChatFirebaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChatFirebaseTests.swift
+//  ChatFirebaseTests
+//
+//  Created by Nguyen Vinh on 1/18/19.
+//  Copyright © 2019 Nguyen Vinh. All rights reserved.
+//
+
+import XCTest
+@testable import ChatFirebase
+
+class ChatFirebaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
